4e51477d07afde0cf650100dffeac6b272ba767d
DeliriumTraemens <oskinnick@gmail.com>
2021-05-28 17:16:09 +0300

    lesson 17 Controllers SecondController

--- src/main/java/org/nick/alishevcourse/springmvc2/controllers/FirstController.java
+++ src/main/java/org/nick/alishevcourse/springmvc2/controllers/FirstController.java
@@ -2,8 +2,11 @@ package org.nick.alishevcourse.springmvc2.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/")
+//@RequestMapping("/first")
 public class FirstController {
 	
 	@GetMapping("/hello")
