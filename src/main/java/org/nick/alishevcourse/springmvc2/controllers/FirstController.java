dd81bd9d41f79949518b7d55c07edfd480a5715d
DeliriumTraemens <oskinnick@gmail.com>
2021-05-30 18:16:09 +0300

    lesson 19 Get Parameters

--- src/main/java/org/nick/alishevcourse/springmvc2/controllers/FirstController.java
+++ src/main/java/org/nick/alishevcourse/springmvc2/controllers/FirstController.java
@@ -4,13 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 @RequestMapping("/")
 //@RequestMapping("/first")
 public class FirstController {
 	
 	@GetMapping("/hello")
-	public String helloPage(){
+	public String helloPage(HttpServletRequest request){
 	return "first/hello";
 	}
 	
