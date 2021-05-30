4dbb4b3416cc6616205ae7178b56846b68618ae3
DeliriumTraemens <oskinnick@gmail.com>
2021-05-30 19:44:00 +0300

    lesson 20 Model

--- src/main/java/org/nick/alishevcourse/springmvc2/controllers/FirstController.java
+++ src/main/java/org/nick/alishevcourse/springmvc2/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package org.nick.alishevcourse.springmvc2.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -25,7 +26,9 @@ public class FirstController {
 //	Для избежания 404 добавляем аргумент required = false @RequestParam(value = "name",required = false)
 	@GetMapping("/hello")
 	public String helloPage(@RequestParam(value = "name",required = false) String name,
-	                        @RequestParam(value = "surname",required = false) String surname){
+	                        @RequestParam(value = "surname",required = false) String surname,
+	                        Model model){
+		model.addAttribute("message", "Hello, "+name+" "+surname);
 		System.out.println("Hello, "+name+" "+surname);
 		return "first/hello";
 	}
